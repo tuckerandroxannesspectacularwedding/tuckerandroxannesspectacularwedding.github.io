66dcb21909af44ed28083ecd99ae6acbd74e54be
Roxanne Coburn <roxanne@Roxannes-MacBook-Pro.local>
2018-09-28 22:14:13 -0400

    website fix

---
+++ wp-login.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-CA">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-CA">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; t + r &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.5' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="/wp-content/uploads/2018/09/cropped-red-heart-painted-in-watercolor-32x32.png" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2018/09/cropped-red-heart-painted-in-watercolor-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2018/09/cropped-red-heart-painted-in-watercolor-180x180.png" />
+<meta name="msapplication-TileImage" content="/wp-content/uploads/2018/09/cropped-red-heart-painted-in-watercolor-270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-ca">
+		<div id="login">
+		<h1><a href="https://en-ca.wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; Back to t + r</a></p>
+	
+	</div>
+
+	
+	<script id="vcv--global-js">
+    function checkPswd() {
+  	console.log("clicked!");
+        var confirmPassword = "parsleysagerosemaryandthyme";
+        var password = document.getElementById("pswd").value;
+        if (password == confirmPassword) {
+             window.location="https://airtable.com/shr194NH2q6LtH022";
+        }
+        else{
+            alert("Passwords do not match!");
+        }
+    } 
+
+jQuery(function () {
+    var header = jQuery(".navigation-top");
+    jQuery(".banner-img").appendTo(".custom-header");
+    jQuery("#modal").prependTo("#page");
+ 	
+   
+   
+    jQuery(window).scroll(function () {
+        var scroll = jQuery(window).scrollTop();
+
+        if (scroll >= 500) {
+            header.addClass("nav-alt").removeClass("nav-before");
+        } else {
+            header.removeClass("nav-alt").addClass("nav-before");
+        }
+
+        var main = jQuery(".custom-header");
+
+        var top_pos = jQuery("#panel1").offset().top;
+        var bottom_pos = jQuery("#panel4").offset().top;
+
+        jQuery(".rsvp-bg").prependTo("#panel1");
+/*
+        jQuery("#close-modal").click(function () {
+            jQuery("#rsvp-modal").hide();
+            jQuery("#modal").hide();
+            jQuery(".page").removeClass("disable-scroll");
+        });
+
+
+        jQuery(".rsvp").click(function () {
+          jQuery("#rsvp-modal").show();
+          jQuery("#modal").show();
+          jQuery(".page").addClass("disable-scroll");
+          jQuery("#rsvp-modal").prependTo("#page");
+          jQuery("#rsvp-modal").addClass("modal");
+
+            if (jQuery(window).width() < 425) {
+                jQuery("#rsvp-modal").offset({
+                    top: top_pos
+                });
+                window.scrollTo(0, top_pos);
+            }
+        });
+
+        jQuery("#rsvp-bottom").click(function () {
+          jQuery("#rsvp-modal").show();
+          jQuery("#modal").show();
+          jQuery("#rsvp-modal").prependTo("#page");
+          jQuery(".page").addClass("disable-scroll");
+					jQuery("#rsvp-modal").addClass("modal");
+
+            if (jQuery(window).width() < 425) {
+                jQuery("#rsvp-modal").offset({
+                    top: bottom_pos
+                });
+                window.scrollTo(0, (bottom_pos));
+            }
+        });
+        */
+    });
+});</script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
